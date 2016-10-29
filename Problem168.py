4f0355b78121730be519ab86a7bf4061960508d3
Jonathan Junxiao Zhu <jonathanzhu1982@hotmail.com>
2016-10-29 13:32:10 -0500

    Create Problem168.py

---
+++ Problem168.py
@@ -0,0 +1,30 @@
+# Given a positive integer, return its corresponding column title as appear in an Excel sheet.
+
+# For example:
+
+#     1 -> A
+#     2 -> B
+#     3 -> C
+#     ...
+#     26 -> Z
+#     27 -> AA
+#     28 -> AB 
+
+
+
+def convertToTitle(self, n):
+        output = []
+        s = ""
+        while n != 0:
+            output.append((n-1)%26 + 65)
+            n = (n - 1) // 26
+        for i in output:
+            s=chr(i)+s
+        return s
+        
+### recursion solution
+#     if n > 26:
+#         print n - (n-1)%26 -1
+#         return (convertToTitle(_,((n - (n-1)%26) -1) / 26) + convertToTitle(_,n%26))        
+#     else:
+#         return (chr((n-1)%26 + 65))        
